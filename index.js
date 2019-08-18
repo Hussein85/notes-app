c38a9c3528b4a3274ea577cf32f017e639ed8de8
Hussein Al Hadad <Hussein1985@hotmail.com>
2019-08-18 20:03:41 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send({ hi: "there" });
+});
+
+// In production or development environment  
+const PORT = process.env.PORT || 5000;
+app.listen(PORT);
