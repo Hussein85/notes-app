c5b781941a453c05ace4372e9acec4126d607cb2
Hussein Al Hadad <Hussein1985@hotmail.com>
2019-09-15 22:02:03 +0200

    changed gitignore file

---
+++ client/build/precache-manifest.3ab5c76fcc4a8ebdcc702a78b2ffcae3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a290e6cdfacbd7773dc3673296568b40",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7bd9cac68ccec52ec5d0",
+    "url": "/static/css/main.f6f67b05.chunk.css"
+  },
+  {
+    "revision": "1a2ee640b613c0b70d17",
+    "url": "/static/js/2.08bdea85.chunk.js"
+  },
+  {
+    "revision": "7bd9cac68ccec52ec5d0",
+    "url": "/static/js/main.a7e7ab34.chunk.js"
+  },
+  {
+    "revision": "db6be01aac7cb1c6d4ff",
+    "url": "/static/js/runtime~main.0033641b.js"
+  },
+  {
+    "revision": "af5a618f5c7047d688139b4269d3e1d3",
+    "url": "/static/media/background.af5a618f.jpg"
+  }
+]);
